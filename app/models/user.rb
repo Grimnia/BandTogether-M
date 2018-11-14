e8a0145ebf7a8d25525bb3bb79a46f7dc1839710
Eduard Fernandez <42663507+Bravo-Bear@users.noreply.github.com>
2018-11-14 17:55:08 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_one :profile, dependent: :destroy 
+  has_one :profile, dependent: :destroy
+  has_many :userpreferences
+  has_many :concerts, through :userpreferences
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
